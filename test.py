76456c91767b45f6deb88bb19ba72a8eb18bcf8d
ijain <irijain@hotmaill.com>
2024-10-13 19:05:07 +0200

    Calculator

---
+++ test.py
@@ -0,0 +1,9 @@
+class TestCalc:
+ def setup(self):
+    self.calc = Calculator
+
+def test_adding_success(self):
+    assert self.calc.adding(1, 1) == 2
+
+def test_multiply_calculate_correctly(self):
+    assert self.calc.multiply(2, 2) == 4
